27bf5d4aa398c4718125b4d22129ed2e2b363b13
Stefano Bernagozzi <stefano.bernagozzi@studio.unibo.it>
2017-07-07 10:57:22 +0200

    Implemented DatabaseDomainClasses

--- Services/DatabaseDomainClasses/User.java
+++ Services/DatabaseDomainClasses/User.java
@@ -4,4 +4,27 @@ package DatabaseDomainClasses;
  * Created by unibo on 07/07/2017.
  */
 public class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
