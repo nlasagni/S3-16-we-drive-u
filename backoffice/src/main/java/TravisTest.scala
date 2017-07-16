a3e402a279555d628dbcbc17632aba076fca921a
Nicola Lasagni <nicola.lasagni@gmail.com>
2017-07-16 11:52:46 +0200

    Added dummy scala file for Travis testing

---
+++ backoffice/src/main/java/TravisTest.scala
@@ -0,0 +1,16 @@
+/**
+  * Created by nicolalasagni on 16/07/2017.
+  */
+object TravisTest {
+
+  def hello = {
+    println("Hello World!")
+  }
+
+}
+
+object Test extends App {
+
+  TravisTest.hello
+
+}
