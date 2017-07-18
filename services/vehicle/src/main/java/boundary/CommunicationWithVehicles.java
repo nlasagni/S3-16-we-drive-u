9dc95932fbfb8b42b80b59bce7f8c75c49a97a1a
Stefano Bernagozzi <stefano.bernagozzi@studio.unibo.it>
2017-07-18 11:44:56 +0200

    added communication with vehicles and find vehicles

---
+++ services/vehicle/src/main/java/boundary/CommunicationWithVehicles.java
@@ -0,0 +1,9 @@
+package boundary;
+
+/**
+ * Created by stefano.bernagozzi on 17/07/2017.
+ */
+public interface CommunicationWithVehicles {
+    public double requestBatteryPercentage(String licensePlate);
+
+}
