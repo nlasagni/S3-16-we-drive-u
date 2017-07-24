daa3d6add04096860aeb74e5cee5f8004adb4aa7
Nicola Lasagni <nicola.lasagni@gmail.com>
2017-07-24 19:36:16 +0200

    Fixed deploy script with test branch set

--- deploy-services.sh
+++ deploy-services.sh
@@ -3,5 +3,5 @@
 if [ "$TRAVIS_REPO_SLUG" == "nlasagni/S3-16-we-drive-u" ] &&
    [ "$TRAVIS_PULL_REQUEST" == "false" ] &&
    [ "$TRAVIS_BRANCH" == "WDU_53_Travis_Setup_Enhancement" ]; then
-  expect deploy-services.exp
+  expect deploy-services.exp $DEPLOY_USER $DEPLOY_PASS $DEPLOY_HOST $TRAVIS_JOB_NUMBER
 fi
