336e4fe53b71e23b9ec4f28ee0103824769bee3d
Nicola Lasagni <nicola.lasagni@gmail.com>
2017-07-24 19:22:10 +0200

    Added deploy script with test branch set

--- deploy-services.sh
+++ deploy-services.sh
@@ -1,14 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if [ "$TRAVIS_REPO_SLUG" == "nlasagni/S3-16-we-drive-u" ] &&
    [ "$TRAVIS_PULL_REQUEST" == "false" ] &&
    [ "$TRAVIS_BRANCH" == "WDU_53_Travis_Setup_Enhancement" ]; then
-
-  echo -e "Deploying services...\n"
-  rm -rf $HOME/services
-  cp -R releases/services $HOME/services
-  cd $HOME
-  export SSHPASS=$DEPLOY_PASS
-  sshpass -e scp services $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH/deploy-$TRAVIS_BUILD_NUMBER
-  echo -e "Services deployed.\n"
+  expect deploy-services.exp
 fi
