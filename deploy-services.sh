cd803936f47c270d2fe75f924e8f3cecdd93c2f4
Nicola Lasagni <nicola.lasagni@gmail.com>
2017-07-25 21:41:10 +0200

    Fixed deploy branch check

--- deploy-services.sh
+++ deploy-services.sh
@@ -2,6 +2,6 @@
 
 if [ "$TRAVIS_REPO_SLUG" == "nlasagni/S3-16-we-drive-u" ] &&
    [ "$TRAVIS_PULL_REQUEST" == "false" ] &&
-   [ "$TRAVIS_BRANCH" == "WDU_53_Travis_Setup_Enhancement" ]; then
+   [ "$TRAVIS_BRANCH" == "master" ]; then
   expect deploy-services.exp $DEPLOY_USER $DEPLOY_PASS $DEPLOY_HOST $TRAVIS_JOB_NUMBER
 fi
