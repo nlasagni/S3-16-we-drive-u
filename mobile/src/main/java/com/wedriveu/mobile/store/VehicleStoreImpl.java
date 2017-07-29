cf5327758d1b53194a55761c17441d7c4bc5caa9
Nicola Lasagni <nicola.lasagni@gmail.com>
2017-07-29 16:31:40 +0200

    Created VehicleStore interface and test and a simple implementation that fails tests

---
+++ mobile/src/main/java/com/wedriveu/mobile/store/VehicleStoreImpl.java
@@ -0,0 +1,21 @@
+package com.wedriveu.mobile.store;
+
+import android.content.Context;
+import com.wedriveu.mobile.model.Vehicle;
+
+/**
+ * @author Nicola Lasagni on 29/07/2017.
+ */
+public class VehicleStoreImpl implements VehicleStore {
+
+    public VehicleStoreImpl(Context context) {}
+
+    @Override
+    public Vehicle getVehicle() {
+        return null;
+    }
+
+    @Override
+    public void storeVehicle(Vehicle vehicle) {}
+
+}
