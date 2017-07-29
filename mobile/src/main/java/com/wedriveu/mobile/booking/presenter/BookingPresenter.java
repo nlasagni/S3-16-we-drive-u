7f50f43ea4b38622804aeffaa329a3fe780d5df7
Nicola Lasagni <nicola.lasagni@gmail.com>
2017-07-29 15:02:21 +0200

    Added BookingPresenter and BookingView interfaces, added BookingPresentationModel for data to be rendered by the view

---
+++ mobile/src/main/java/com/wedriveu/mobile/booking/presenter/BookingPresenter.java
@@ -0,0 +1,12 @@
+package com.wedriveu.mobile.booking.presenter;
+
+/**
+ * Created by nicolalasagni on 28/07/2017.
+ */
+public interface BookingPresenter {
+
+    void onAcceptButtonClick();
+
+    void onDeclineButtonClick();
+
+}
---
+++ mobile/src/main/java/com/wedriveu/mobile/booking/view/BookingView.java
@@ -0,0 +1,12 @@
+package com.wedriveu.mobile.booking.view;
+
+import com.wedriveu.mobile.booking.presenter.model.BookingPresentationModel;
+
+/**
+ * Created by nicolalasagni on 28/07/2017.
+ */
+public interface BookingView {
+
+    void renderView(BookingPresentationModel presentationModel);
+
+}
