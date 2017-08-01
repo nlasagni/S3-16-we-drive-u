fb9d3f020549c09dabff59785b68ec5f37a4142a
Michele Donati <michele.donati5@studio.unibo.it>
2017-08-01 16:43:59 +0200

    Removed Main instructions because of tests

--- vehicle/src/main/scala/com/wedriveu/vehicle/app/Main.scala
+++ vehicle/src/main/scala/com/wedriveu/vehicle/app/Main.scala
@@ -8,22 +8,7 @@ import com.wedriveu.vehicle.control.{VehicleControl, VehicleControlImpl}
 /**
   * Created by Michele on 31/07/2017.
   */
-object Main extends App{
-  val nVehicles: Int = 3
-  val minorBound: Int = 30
-  val maxBound: Int = 101
-  val vehicle: String = "veicolo"
-  val state: String = "available"
-  val latitude: Double = 10.0
-  val longitude: Double = 10.0
-
-  var vehicles: Array[VehicleControl] = new Array[VehicleControl](nVehicles)
-  var a = 0
-  for (a <- 0 until vehicles.length) {
-    var randomBattery: Double = ThreadLocalRandom.current().nextDouble(minorBound, maxBound)
-    vehicles(a) = new VehicleControlImpl(vehicle + (a+1), state, new Position(latitude, longitude), randomBattery)
-    vehicles(a).startVehicleEngine()
-  }
-
+object Main extends App {
+      /* empty */
 }
 
