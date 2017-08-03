7242cfd49b340126526fb19afc7084ca7520995b
Michele Donati <michele.donati5@studio.unibo.it>
2017-08-03 18:16:03 +0200

    Removed Drain Battery events, joined them in movement events

--- vehicle/src/main/scala/com/wedriveu/vehicle/control/VehicleBehavioursImpl.scala
+++ vehicle/src/main/scala/com/wedriveu/vehicle/control/VehicleBehavioursImpl.scala
@@ -1,6 +1,6 @@
 package com.wedriveu.vehicle.control
 
-import com.wedriveu.services.shared.utilities.Log
+import com.wedriveu.services.shared.utilities.{Log}
 import com.wedriveu.vehicle.entity.{Position, SelfDrivingVehicle}
 
 /**
@@ -9,9 +9,6 @@ import com.wedriveu.vehicle.entity.{Position, SelfDrivingVehicle}
 
 /** This is the vehicle behaviours models capturing different events. */
 trait VehicleBehaviours {
-  /** This models the behaviour of the vehicle's battery draining. */
-  def drainBattery() : Unit
-
   /** This models the behaviour of the vehicle's movement and position changing.
     *
     * @param position Indicates the final position we want for the vehicle.
@@ -27,55 +24,62 @@ trait VehicleBehaviours {
 
 }
 
-class VehicleBehavioursImpl(vehicle: SelfDrivingVehicle) extends VehicleBehaviours {
-   var notRecharging: Boolean = true
+class VehicleBehavioursImpl(selfDrivingVehicle: SelfDrivingVehicle) extends VehicleBehaviours {
    val zeroBattery: Double = 0.0
    val batteryThreshold: Double = 20.0
    val stateRecharging: String = "recharging"
-   val batteryToConsume: Double = 1.0
+   val batteryToConsume: Double = 0.14
    val newPositionIsTheSame: String = "The position given is the same, the vehicle doesn't move"
-   val conversionInMillis: Int = 3600 * 1000
    val timeOfJourney: Long = 0.0.asInstanceOf[Long]
-   val timeStep: Long = 1000.0.asInstanceOf[Long]
+   val timeStep: Long = 10.0.asInstanceOf[Long]
+   val timeStepBatteryConsumed: Double = 100.0
+   val conversionInSeconds: Double = 3600.0
 
-   override def drainBattery() = {
-    if(vehicle.battery <= zeroBattery){
-      Log.log("Battery of " + vehicle.plate + " = " + vehicle.battery.toString)
+   var notRecharging: Boolean = true
+   var timePassedForBattery: Double = .0
+
+   private def drainBattery(): Unit = {
+    if(selfDrivingVehicle.battery <= zeroBattery){
+      Log.log("Battery of " + selfDrivingVehicle.plate + " = " + selfDrivingVehicle.battery.toString)
     }
     else{
-      if((vehicle.battery-batteryToConsume) < zeroBattery) {
-        vehicle.battery = zeroBattery
+      if((selfDrivingVehicle.battery-batteryToConsume) <= zeroBattery) {
+        selfDrivingVehicle.battery = zeroBattery
       }
       else {
         // Depends on vehicle speed
-        vehicle.battery -= batteryToConsume
+        selfDrivingVehicle.battery -= batteryToConsume
       }
-      if(vehicle.battery <= batteryThreshold && notRecharging) {
-        vehicle.state = stateRecharging
+      if(selfDrivingVehicle.battery <= batteryThreshold && notRecharging) {
+        selfDrivingVehicle.state = stateRecharging
         //This will be set to True when the vehicle recharges
         notRecharging = false
-        Log.log("Vehicle " + vehicle.plate + " state changed to: " + vehicle.state)
+        Log.log("Vehicle " + selfDrivingVehicle.plate + " state changed to: " + selfDrivingVehicle.state)
       }
-      Log.log("Battery of " + vehicle.plate + " = " + vehicle.battery.toString)
+      Log.log("Battery of " + selfDrivingVehicle.plate + " = " + selfDrivingVehicle.battery.toString)
     }
   }
 
   //This algorithm calculates the distance in Km between the points, then estimates the journey time and calculates
   //the coordinates reached during the journey.
   override def movementAndPositionChange(position: Position): Unit = {
-    if (vehicle.position.equals(position)) {
+    if (selfDrivingVehicle.position.equals(position)) {
       Log.log(newPositionIsTheSame)
     }
     else {
-      val distanceInKm: Double = vehicle.position.getDistanceInKm(position)
-      val estimatedJourneyTimeInMilliseconds: Long =
-        ((distanceInKm / vehicle.speed) * conversionInMillis).asInstanceOf[Long]
-      val deltaLat: Double = position.latitude - vehicle.position.latitude
-      val deltaLon: Double = position.longitude - vehicle.position.longitude
-      for(time <- timeOfJourney to estimatedJourneyTimeInMilliseconds by timeStep) {
-        calculateMovement(time, estimatedJourneyTimeInMilliseconds, deltaLat, deltaLon)
-        if((time + timeStep) > estimatedJourneyTimeInMilliseconds) {
-          calculateMovement(estimatedJourneyTimeInMilliseconds, estimatedJourneyTimeInMilliseconds, deltaLat, deltaLon)
+      timePassedForBattery = timeStepBatteryConsumed
+      val distanceInKm: Double = selfDrivingVehicle.position.getDistanceInKm(position)
+      Log.log("Distance in Km is:" + distanceInKm)
+      val estimatedJourneyTimeInSeconds: Long =
+        ((distanceInKm / selfDrivingVehicle.speed) * conversionInSeconds).asInstanceOf[Long]
+      Log.log("Time in seconds is:" + estimatedJourneyTimeInSeconds)
+      for(time <- timeOfJourney to estimatedJourneyTimeInSeconds by timeStep) {
+        val deltaLat: Double = position.latitude - selfDrivingVehicle.position.latitude
+        val deltaLon: Double = position.longitude - selfDrivingVehicle.position.longitude
+        calculateMovement(time, estimatedJourneyTimeInSeconds, deltaLat, deltaLon)
+        if((time + timeStep) > estimatedJourneyTimeInSeconds) {
+          calculateMovement(estimatedJourneyTimeInSeconds, estimatedJourneyTimeInSeconds, deltaLat, deltaLon)
+          Log.log("Final position should be: " + position.latitude + " , " + position.longitude)
         }
       }
     }
@@ -87,13 +91,21 @@ class VehicleBehavioursImpl(vehicle: SelfDrivingVehicle) extends VehicleBehaviou
   }
 
   private def calculateMovement(time: Long,
-                                estimatedJourneyTimeInMilliseconds: Long,
+                                estimatedJourneyTimeInSeconds: Long,
                                 deltaLat: Double,
                                 deltaLon: Double): Unit = {
-    val elapsedTime: Double = time / estimatedJourneyTimeInMilliseconds
-    var latInter: Double = vehicle.position.latitude + deltaLat * elapsedTime
-    var lonInter: Double = vehicle.position.longitude + deltaLon * elapsedTime
-    vehicle.position = new Position(latInter, lonInter)
+    val elapsedTime: Double = time.asInstanceOf[Double] / estimatedJourneyTimeInSeconds
+    var latInter: Double = selfDrivingVehicle.position.latitude + deltaLat * elapsedTime
+    var lonInter: Double = selfDrivingVehicle.position.longitude + deltaLon * elapsedTime
+    selfDrivingVehicle.position = new Position(latInter, lonInter)
+    if(time != 0.0 && ((time / timePassedForBattery)%1) == 0.0) {
+      drainBattery()
+      timePassedForBattery += timeStepBatteryConsumed
+    }
+    Log.log("New Position is: "
+      + selfDrivingVehicle.position.latitude
+      + " , "
+      + selfDrivingVehicle.position.longitude)
   }
 
 }
--- vehicle/src/main/scala/com/wedriveu/vehicle/entity/VehicleCreator.scala
+++ vehicle/src/main/scala/com/wedriveu/vehicle/entity/VehicleCreator.scala
@@ -3,16 +3,25 @@ package com.wedriveu.vehicle.entity
 import java.math.BigInteger
 import java.security.SecureRandom
 
+import com.wedriveu.vehicle.control.{VehicleControl, VehicleControlImpl}
+
 /**
   * Created by Michele on 02/08/2017.
   */
 class VehicleCreator(battery: Double, doBreak: Boolean, doNotBreak: Boolean) {
   val randomNumber: SecureRandom = new SecureRandom()
-  var randomPlate: String = randomLicensePlateGenerator()
+  val randomPlate: String = randomLicensePlateGenerator()
+  val initialState: String = "available"
+  val initialLatitude: Double = 44.1454528
+  val initialLongitude: Double = 12.2474513
+  val initialPosition: Position = new Position(initialLatitude, initialLongitude)
 
+  val newVehicle: VehicleControl = new VehicleControlImpl(randomPlate, initialState, initialPosition, battery)
+  newVehicle.startVehicleEngine()
+  newVehicle.subscribeToMovementAndChangePositionEvents()
 
-  private def randomLicensePlateGenerator(): String ={
-    new BigInteger(130, randomNumber).toString(32)
+  private def randomLicensePlateGenerator(): String = {
+    new BigInteger(128, randomNumber).toString(32)
   }
 
 }
