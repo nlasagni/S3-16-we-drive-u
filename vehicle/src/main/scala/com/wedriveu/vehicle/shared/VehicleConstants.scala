e50c46f5ea220c15e1c03e20dc72f1b1c0b3a27b
Michele Donati <michele.donati5@studio.unibo.it>
2017-08-06 15:47:02 +0200

    Added Todo comment

--- vehicle/src/main/scala/com/wedriveu/vehicle/shared/VehicleConstants.scala
+++ vehicle/src/main/scala/com/wedriveu/vehicle/shared/VehicleConstants.scala
@@ -9,5 +9,6 @@ object VehicleConstants {
   final val stateBroken: String = "broken"
   final val stateRecharging: String = "recharging"
   final val stateAvailable: String ="available"
+  //TODO Put this constants in the Nicola's shared module, after it's push.
 
 }
