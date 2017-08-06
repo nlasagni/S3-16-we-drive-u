8d676f890b2bd40d4de0aca9a1de95ff5517ec2b
Michele Donati <michele.donati5@studio.unibo.it>
2017-08-06 15:35:00 +0200

    Created shared package inside vehicle module

---
+++ vehicle/src/main/scala/com/wedriveu/vehicle/shared/VehicleConstants.scala
@@ -0,0 +1,13 @@
+package com.wedriveu.vehicle.shared
+
+/**
+  * Created by Michele on 06/08/2017.
+  */
+trait VehicleConstants {}
+
+object VehicleConstants {
+  final val stateBroken: String = "broken"
+  final val stateRecharging: String = "recharging"
+  final val stateAvailable: String ="available"
+
+}
