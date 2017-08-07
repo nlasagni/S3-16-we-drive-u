fdc8ddc0e99848974420ed7892e718f42a50ca02
Stefano Bernagozzi <stefano.bernagozzi@studio.unibo.it>
2017-08-07 16:05:17 +0200

    refactored analyticsStoreTest

--- services/analytics/src/test/java/com/wedriveu/services/analytics/entity/AnalyticsStoreTest.java
+++ services/analytics/src/test/java/com/wedriveu/services/analytics/entity/AnalyticsStoreTest.java
@@ -2,7 +2,6 @@ package com.wedriveu.services.analytics.entity;
 
 import com.wedriveu.services.shared.entity.EntityListStoreStrategy;
 import com.wedriveu.services.shared.entity.JsonFileEntityListStoreStrategyImpl;
-import jdk.nashorn.internal.runtime.regexp.joni.constants.OPCode;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -20,33 +19,28 @@ import static org.junit.Assert.*;
 public class AnalyticsStoreTest {
     private static final String DATABASE_FILE_NAME = AnalyticsStoreTest.class.getSimpleName() + ".json";
     private static final String VEHICLE_1_LICENSE_PLATE = "vehicle1";
-    private static final String VEHICLE_2_LICENSE_PLATE = "vehicle2";
     private static final String VEHICLE_NOT_FOUND_LICENSE_PLATE = "vehicle3";
 
     private AnalyticsStore analyticsStore;
-    private List<AnalyticsVehicle> vehicles;
+    private AnalyticsVehicle vehicle;
 
     @Before
     public void setUp() throws Exception {
         EntityListStoreStrategy<AnalyticsVehicle> storeStrategy =
                 new JsonFileEntityListStoreStrategyImpl<>(AnalyticsVehicle.class, DATABASE_FILE_NAME);
         analyticsStore = new AnalyticsStoreImpl(storeStrategy);
-        vehicles = new ArrayList<>();
-        vehicles.add(new AnalyticsVehicle(VEHICLE_1_LICENSE_PLATE, "available"));
-        vehicles.add(new AnalyticsVehicle(VEHICLE_2_LICENSE_PLATE, "available"));
+        vehicle= new AnalyticsVehicle(VEHICLE_1_LICENSE_PLATE, "available");
     }
 
     @Test
     public void addVehicle() throws Exception {
-        AnalyticsVehicle vehicle = vehicles.get(0);
-        boolean correctInsertionSuccess = analyticsStore.addVehicle(vehicle.getLicensePlate(), vehicle.getLicensePlate());
+        boolean correctInsertionSuccess = insertVehicleIntoDatabase();
         assertTrue(correctInsertionSuccess);
     }
 
     @Test
     public void getVehicleByLicensePlate() throws Exception {
-        AnalyticsVehicle vehicle = vehicles.get(0);
-        boolean insertionSucceded = analyticsStore.addVehicle(vehicle.getLicensePlate(), vehicle.getStatus());
+        boolean insertionSucceded = insertVehicleIntoDatabase();
         Optional<AnalyticsVehicle> vehicleFromStore = analyticsStore.getVehicleByLicensePlate(vehicle.getLicensePlate());
         Optional<AnalyticsVehicle> vehicleNotFound = analyticsStore.getVehicleByLicensePlate(VEHICLE_NOT_FOUND_LICENSE_PLATE);
         assertTrue(insertionSucceded &&
@@ -57,8 +51,7 @@ public class AnalyticsStoreTest {
 
     @Test
     public void updateVehicle() throws Exception {
-        AnalyticsVehicle vehicle = vehicles.get(0);
-        boolean insertionSucceded = analyticsStore.addVehicle(vehicle.getLicensePlate(), vehicle.getStatus());
+        boolean insertionSucceded = insertVehicleIntoDatabase();
         final String NEW_STATUS = "broken";
         boolean updateSucceded = analyticsStore.updateVehicle(vehicle.getLicensePlate(),NEW_STATUS);
         Optional<AnalyticsVehicle> vehicleFromStore = analyticsStore.getVehicleByLicensePlate(vehicle.getLicensePlate());
@@ -69,4 +62,7 @@ public class AnalyticsStoreTest {
 
     }
 
+    private boolean insertVehicleIntoDatabase() {
+        return analyticsStore.addVehicle(vehicle.getLicensePlate(), vehicle.getLicensePlate());
+    }
 }
