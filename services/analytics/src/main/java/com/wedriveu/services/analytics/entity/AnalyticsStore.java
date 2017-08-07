129a6821dde9c272edab57fc3c27d338923af228
Stefano Bernagozzi <stefano.bernagozzi@studio.unibo.it>
2017-08-07 12:34:07 +0200

    edited interface and its javadoc

--- services/analytics/src/main/java/com/wedriveu/services/analytics/entity/AnalyticsStore.java
+++ services/analytics/src/main/java/com/wedriveu/services/analytics/entity/AnalyticsStore.java
@@ -1,5 +1,7 @@
 package com.wedriveu.services.analytics.entity;
 
+import java.util.Optional;
+
 /**
  * @author Stefano Bernagozzi
  * This inteface models the <em>com.wedriveu.services.analytics.entity.AnalyticsVehicle' database store</em>.
@@ -12,26 +14,28 @@ public interface AnalyticsStore {
      * @param licensePlate the string that represents the license plate of the <em>AnalyticsVehicle</em> that wants to be returned
      * @return a <em>AnalyticsVehicle</em> that contains the license plate and the status of the vehicle requested
      */
-    public AnalyticsVehicle getVehicleByLicensePlate(String licensePlate);
+    Optional<AnalyticsVehicle> getVehicleByLicensePlate(String licensePlate);
 
     /**
      * adds a <em>AnalyticsVehicle</em> to StoreEntities
      * @param licensePlate the license plate of the <em>AnalyticsVehicle</em> that wants to be added
      * @param status the current status of the <em>AnalyticsVehicle</em> that wants to be added
+     * @return A boolean indicating the success or the failure of this operation.
      */
-    public void addVehicle(String licensePlate, String status);
+    boolean addVehicle(String licensePlate, String status);
 
     /**
      * update the status of a <em>AnalyticsVehicle</em>
      * @param licensePlate the license plate of the <em>AnalyticsVehicle</em> that wants to be updated
      * @param status the current status of the <em>AnalyticsVehicle</em> that wants to be updated
+     * @return A boolean indicating the success or the failure of this operation.
      */
-    public void updateVehicle(String licensePlate, String status);
+    boolean updateVehicle(String licensePlate, String status);
 
     /**
      * clear the database
      */
-    public void clear();
+    void clear();
 
 }
 
