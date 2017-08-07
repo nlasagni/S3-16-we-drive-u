5ff318cc59bb3a41bfa39c6fa3d65ff0f6d5c4ef
Michele Donati <michele.donati5@studio.unibo.it>
2017-08-07 18:40:46 +0200

    Refactoring of the Latch code

--- vehicle/src/main/scala/com/wedriveu/vehicle/simulation/RechargingLatchManager.scala
+++ vehicle/src/main/scala/com/wedriveu/vehicle/simulation/RechargingLatchManager.scala
@@ -1,5 +1,7 @@
 package com.wedriveu.vehicle.simulation
 
+import java.util.concurrent.{CountDownLatch, TimeUnit}
+
 import com.wedriveu.vehicle.entity.SelfDrivingVehicle
 
 /**
@@ -12,10 +14,16 @@ trait RechargingLatchManager {
   def startLatchedThread(): Unit
 }
 class RechargingLatchManagerImpl(selfDrivingVehicle: SelfDrivingVehicle) extends RechargingLatchManager {
+  val tenSecondsOfRecharge: Int = 10
+  val maxTimeOfAwaiting: Long = 15L
+  var countdownLatch: CountDownLatch = new CountDownLatch(tenSecondsOfRecharge)
 
-  val rechargingLatchedThread: RechargingLatchedThread = new RechargingLatchedThread
+  val rechargingLatchedThread: RechargingLatchedThread =
+    new RechargingLatchedThread(selfDrivingVehicle: SelfDrivingVehicle, countdownLatch: CountDownLatch)
+
   override def startLatchedThread(): Unit = {
-      rechargingLatchedThread.performRechargeSimulatorTask(selfDrivingVehicle)
+    rechargingLatchedThread.start()
+    countdownLatch.await(maxTimeOfAwaiting, TimeUnit.SECONDS)
   }
 
 }
--- vehicle/src/main/scala/com/wedriveu/vehicle/simulation/RechargingLatchedThread.scala
+++ vehicle/src/main/scala/com/wedriveu/vehicle/simulation/RechargingLatchedThread.scala
@@ -9,29 +9,13 @@ import com.wedriveu.vehicle.shared.VehicleConstants
   * @author Michele Donati on 07/08/2017.
   */
 
-/** This trait models the CountDownLatch Thread that is used for simulating the recharging process of the vehicle,
-  * for about 10 seconds.
-  */
-trait RechargingLatched {
-  /** This method permits to run the CoundDownLatch Thread.
-    *
-    * @param vehicle Indicates the vehicle associated.
-    */
-  def performRechargeSimulatorTask(vehicle: SelfDrivingVehicle): Unit
-}
-
-class RechargingLatchedThread() extends Thread with RechargingLatched {
+class RechargingLatchedThread(selfDrivingVehicle: SelfDrivingVehicle, countdownLatch: CountDownLatch) extends Thread {
   val oneSecondInMillis: Int = 1000
-  val tenSecondsOfRecharge: Int = 10
-  val maxTimeOfAwaiting: Long = 15L
-
-  var countdownLatch: CountDownLatch = null
-  var vehicle: SelfDrivingVehicle = null
 
   override def run(): Unit = {
     for(i <- 0 until 10 by 1){
       try {
-        if(!vehicle.getSate().equals(VehicleConstants.stateBroken)) {
+        if(!selfDrivingVehicle.getSate().equals(VehicleConstants.stateBroken)) {
           Thread.sleep(oneSecondInMillis)
         }
         else {
@@ -42,15 +26,8 @@ class RechargingLatchedThread() extends Thread with RechargingLatched {
         countdownLatch.countDown()
       }
     }
-    vehicle.battery = VehicleConstants.maxBatteryValue
-    vehicle.setState(VehicleConstants.stateAvailable)
-  }
-
-  override def performRechargeSimulatorTask(vehicle: SelfDrivingVehicle): Unit = {
-    this.vehicle = vehicle
-    countdownLatch = new CountDownLatch(tenSecondsOfRecharge)
-    this.start()
-    countdownLatch.await(maxTimeOfAwaiting, TimeUnit.SECONDS)
+    selfDrivingVehicle.battery = VehicleConstants.maxBatteryValue
+    selfDrivingVehicle.setState(VehicleConstants.stateAvailable)
   }
 
 }
