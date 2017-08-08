28996b930ba1c4213e79dcee99f683ff8b7968e3
Nicola Lasagni <nicola.lasagni@gmail.com>
2017-08-09 00:11:10 +0200

    Refactored AuthenticationService Constants

--- services/authentication/src/main/java/com/wedriveu/services/authentication/util/Constants.java
+++ services/authentication/src/main/java/com/wedriveu/services/authentication/util/Constants.java
@@ -3,7 +3,7 @@ package com.wedriveu.services.authentication.util;
 /**
  * @author Nicola Lasagni on 08/08/2017.
  */
-public interface Constants extends com.wedriveu.services.shared.utilities.Constants {
+public interface Constants extends com.wedriveu.shared.util.Constants {
 
     String USERNAME_PASSWORD_MISSING = "username or password missing";
     String USERNAME_PASSWORD_WRONG = "wrong username or password";
