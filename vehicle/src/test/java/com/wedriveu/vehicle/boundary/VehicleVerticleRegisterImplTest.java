900d6ce1feaf10aae182be1f120d85eb3c374b7a
Michele Donati <michele.donati5@studio.unibo.it>
2017-08-11 17:21:02 +0200

    Implemented Comunicaion tests

---
+++ vehicle/src/test/java/com/wedriveu/vehicle/boundary/VehicleVerticleRegisterImplTest.java
@@ -0,0 +1,167 @@
+package com.wedriveu.vehicle.boundary;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wedriveu.services.shared.rabbitmq.RabbitMQConfig;
+import com.wedriveu.services.shared.utilities.Constants;
+import com.wedriveu.shared.entity.RegisterToServiceRequest;
+import com.wedriveu.shared.entity.RegisterToServiceResponse;
+import com.wedriveu.shared.utils.Log;
+import com.wedriveu.shared.utils.Position;
+import com.wedriveu.vehicle.control.VehicleControl;
+import com.wedriveu.vehicle.control.VehicleControlImpl;
+import com.wedriveu.vehicle.shared.EventBusConstants$;
+import com.wedriveu.vehicle.shared.Exchanges$;
+import com.wedriveu.vehicle.shared.RoutingKeys$;
+import com.weriveu.vehicle.boundary.VehicleVerticleRegisterImpl;
+import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.eventbus.MessageConsumer;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.vertx.rabbitmq.RabbitMQClient;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Michele Donati on 10/08/2017.
+ */
+
+@RunWith(VertxUnitRunner.class)
+public class VehicleVerticleRegisterImplTest {
+
+    private static final String TAG = VehicleVerticleRegisterImplTest.class.getSimpleName();
+    private static final String JSON_QUEUE_KEY = "queue";
+    private static final String EVENT_BUS_ADDRESS = VehicleVerticleRegisterImplTest.class.getSimpleName();
+
+    private EventBusConstants$ eventBusConstants = EventBusConstants$.MODULE$;
+    private Exchanges$ exchanges = Exchanges$.MODULE$;
+    private RoutingKeys$ routingKeys = RoutingKeys$.MODULE$;
+    private Vertx vertx;
+    private EventBus eventBus;
+    private RabbitMQClient rabbitMQClient;
+    private ObjectMapper objectMapper;
+    private VehicleVerticleRegisterImpl vehicleVerticle;
+    private String requestId;
+    private VehicleControl vehicleControl;
+    private String license = "VEHICLE2";
+    private String[] licenseList = {"VEHICLE1", "VEHICLE3", "VEHICLE4", "VEHICLE5"};
+    private String state = "available";
+    private Position position = new Position(44.1454528, 12.2474513);
+    private double battery = 100.0;
+    private double speed = 50.0;
+    private VehicleStopView stopUi = new VehicleStopViewImpl(1);
+    private boolean debugVar = false;
+
+    @Before
+    public void setUp(TestContext context) throws Exception {
+        vertx = Vertx.vertx();
+        eventBus = vertx.eventBus();
+        objectMapper = new ObjectMapper();
+        vehicleControl = new VehicleControlImpl(license, state, position, battery, speed, stopUi, debugVar);
+        vehicleVerticle = new VehicleVerticleRegisterImpl(vehicleControl);
+        setUpAsyncComponents(context);
+    }
+
+    private void setUpAsyncComponents(TestContext context) {
+        Async async = context.async(5);
+        JsonObject config = new JsonObject();
+        config.put(Constants.RabbitMQ.ConfigKey.HOST, Constants.RabbitMQ.Broker.HOST);
+        config.put(Constants.RabbitMQ.ConfigKey.PASSWORD, Constants.RabbitMQ.Broker.PASSWORD);
+        config.put(Constants.RabbitMQ.ConfigKey.PORT, Constants.RabbitMQ.Broker.PORT);
+        rabbitMQClient = io.vertx.rabbitmq.RabbitMQClient.create(vertx, config);
+        rabbitMQClient.start(onStart -> {
+            rabbitMQClient.queueDeclareAuto(onQueueDeclare -> {
+                requestId = onQueueDeclare.result().getString(JSON_QUEUE_KEY);
+                    rabbitMQClient.queueBind(requestId, exchanges.VEHICLE(), routingKeys.REGISTER_REQUEST(), onQueueBind ->{
+                        vertx.deployVerticle(vehicleVerticle, context.asyncAssertSuccess(onDeploy -> {
+                            System.out.println("TEST: IL VEICOLO DEPLOYATO");
+                            async.complete();}
+                        ));
+                        System.out.println("TEST: QUEUE " + requestId + " BINDATA" );
+                        async.countDown();
+                        context.assertTrue(onQueueBind.succeeded());
+                    });
+                    System.out.println("TEST: EXCHANGE DICHIARATO");
+                    async.countDown();
+                context.assertTrue(onQueueDeclare.succeeded());
+                System.out.println("TEST: QUEUE DICHIARATA");
+                async.countDown();
+            });
+            async.countDown();
+        });
+        async.awaitSuccess();
+    }
+
+    @After
+    public void tearDown(TestContext context) throws Exception {
+        rabbitMQClient.stop(context.asyncAssertSuccess());
+    }
+
+    @Test
+    public void registerToService(TestContext context) throws Exception {
+        checkVehicleRequest(context);
+    }
+
+    private void checkVehicleRequest(TestContext context) {
+        final Async async = context.async();
+        System.out.println("TEST: CHECK VEHICLE REQUEST");
+        rabbitMQClient.basicConsume(requestId, EVENT_BUS_ADDRESS, onGet -> {
+            System.out.println("TEST: BASIC CONSUME COMPLETATA");
+            MessageConsumer<JsonObject> consumer = eventBus.consumer(EVENT_BUS_ADDRESS, msg -> {
+                System.out.println("TEST: CONSUMO MESSAGGIO");
+                JsonObject requestJson = new JsonObject(msg.body().getString(eventBusConstants.BODY()));
+                Log.info(TAG, requestJson.toString());
+                RegisterToServiceRequest request = requestJson.mapTo(RegisterToServiceRequest.class);
+                System.out.println("TEST: La request = " + request.getLicense());
+                RegisterToServiceResponse response = new RegisterToServiceResponse();
+                response.setRegisterOk(checkLicenseList(request.getLicense()));
+                System.out.println("TEST: La response = " + response.getRegisterOk());
+                sendResponse(response, context);
+            });
+            consumer.exceptionHandler(event -> {
+                context.fail(event.getCause());
+            });
+        });
+        vertx.setTimer(8000, onTime -> {
+            System.out.println("TEST: ASYNC COMPLETE");
+            async.complete();});
+        async.awaitSuccess();
+    }
+
+    private boolean checkLicenseList(String license) {
+        boolean ok = true;
+        for(int i = 0; i < licenseList.length; i++) {
+            if(license.equals(licenseList[i])){
+                ok = false;
+                return ok;
+            }
+        }
+        return ok;
+    }
+
+    private void sendResponse(RegisterToServiceResponse response, TestContext context) {
+        try {
+            String responseString = objectMapper.writeValueAsString(response);
+            System.out.println("HO FATTO LA RESPONSE = " + responseString);
+            JsonObject responseJson = new JsonObject();
+            responseJson.put(eventBusConstants.BODY(), responseString);
+            System.out.println("IL JSON = " + responseJson.toString());
+            System.out.println("License = " + license);
+            rabbitMQClient.basicPublish(exchanges.VEHICLE(),
+                    String.format(routingKeys.REGISTER_RESPONSE(), license),
+                    responseJson,
+                    onPublish -> {
+                        context.assertTrue(onPublish.succeeded());
+                        System.out.println("SEND RIUSCITA");
+                    });
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ vehicle/src/test/java/com/wedriveu/vehicle/boundary/VehicleVerticleUpdateImplTest.java
@@ -0,0 +1,131 @@
+package com.wedriveu.vehicle.boundary;
+
+import com.wedriveu.services.shared.utilities.Constants;
+import com.wedriveu.shared.entity.UpdateToService;
+import com.wedriveu.shared.utils.Log;
+import com.wedriveu.shared.utils.Position;
+import com.wedriveu.vehicle.control.VehicleControl;
+import com.wedriveu.vehicle.control.VehicleControlImpl;
+import com.wedriveu.vehicle.shared.EventBusConstants$;
+import com.wedriveu.vehicle.shared.Exchanges$;
+import com.wedriveu.vehicle.shared.RoutingKeys$;
+import com.weriveu.vehicle.boundary.VehicleVerticleUpdateImpl;
+import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.eventbus.MessageConsumer;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.vertx.rabbitmq.RabbitMQClient;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import scala.collection.script.Update;
+
+/**
+ * @author Michele Donati on 11/08/2017.
+ */
+
+@RunWith(VertxUnitRunner.class)
+public class VehicleVerticleUpdateImplTest {
+
+    private static final String TAG = VehicleVerticleUpdateImplTest.class.getSimpleName();
+    private static final String JSON_QUEUE_KEY = "queue";
+    private static final String EVENT_BUS_ADDRESS = VehicleVerticleUpdateImplTest.class.getCanonicalName();
+    private static final String FAILURE_MESSAGE = "The vehicle is not broken/stolen";
+
+    private EventBusConstants$ eventBusConstants = EventBusConstants$.MODULE$;
+    private Exchanges$ exchanges = Exchanges$.MODULE$;
+    private RoutingKeys$ routingKeys = RoutingKeys$.MODULE$;
+    private Vertx vertx;
+    private EventBus eventBus;
+    private RabbitMQClient rabbitMQClient;
+    private VehicleVerticleUpdateImpl vehicleVerticle;
+    private String requestId;
+    private VehicleControl vehicleControl;
+    private String license = "VEHICLE5";
+    private String state = "available";
+    private Position position = new Position(44.1454528, 12.2474513);
+    private double battery = 100.0;
+    private double speed = 50.0;
+    private VehicleStopView stopUi = new VehicleStopViewImpl(1);
+    private boolean debugVar = false;
+
+    @Before
+    public void setUp(TestContext context) throws Exception {
+        vertx = Vertx.vertx();
+        eventBus = vertx.eventBus();
+        vehicleControl = new VehicleControlImpl(license, state, position, battery, speed, stopUi, debugVar);
+        vehicleVerticle = new VehicleVerticleUpdateImpl(vehicleControl);
+        setUpAsyncComponents(context);
+    }
+
+    private void setUpAsyncComponents(TestContext context) {
+        Async async = context.async(5);
+        JsonObject config = new JsonObject();
+        config.put(Constants.RabbitMQ.ConfigKey.HOST, Constants.RabbitMQ.Broker.HOST);
+        config.put(Constants.RabbitMQ.ConfigKey.PASSWORD, Constants.RabbitMQ.Broker.PASSWORD);
+        config.put(Constants.RabbitMQ.ConfigKey.PORT, Constants.RabbitMQ.Broker.PORT);
+        rabbitMQClient = io.vertx.rabbitmq.RabbitMQClient.create(vertx, config);
+        rabbitMQClient.start(onStart -> {
+            rabbitMQClient.queueDeclareAuto(onQueueDeclare -> {
+                requestId = onQueueDeclare.result().getString(JSON_QUEUE_KEY);
+                rabbitMQClient.queueBind(requestId, exchanges.VEHICLE(), routingKeys.VEHICLE_UPDATE(), onQueueBind ->{
+                    vertx.deployVerticle(vehicleVerticle, context.asyncAssertSuccess(onDeploy -> {
+                        System.out.println("TEST: IL VEICOLO DEPLOYATO");
+                        async.complete();}
+                    ));
+                    System.out.println("TEST: QUEUE " + requestId + " BINDATA" );
+                    async.countDown();
+                    context.assertTrue(onQueueBind.succeeded());
+                });
+                async.countDown();
+                context.assertTrue(onQueueDeclare.succeeded());
+                System.out.println("TEST: QUEUE DICHIARATA");
+                async.countDown();
+            });
+            async.countDown();
+        });
+        async.awaitSuccess();
+    }
+
+    @After
+    public void tearDown(TestContext context) throws Exception {
+        rabbitMQClient.stop(context.asyncAssertSuccess());
+    }
+
+    @Test
+    public void sendUpdate(TestContext context) throws Exception {
+        checkVehicleUpdate(context);
+    }
+
+    private void checkVehicleUpdate(TestContext context) {
+        final Async async = context.async();
+        System.out.println("TEST: CHECK VEHICLE UPDATE");
+        rabbitMQClient.basicConsume(requestId, EVENT_BUS_ADDRESS, onGet -> {
+            System.out.println("TEST: BASIC CONSUME COMPLETATA");
+            MessageConsumer<JsonObject> consumer = eventBus.consumer(EVENT_BUS_ADDRESS, msg -> {
+                System.out.println("TEST: CONSUMO MESSAGGIO");
+                JsonObject updateJson = new JsonObject(msg.body().getString(eventBusConstants.BODY()));
+                Log.info(TAG, updateJson.toString());
+                UpdateToService updateArrived = updateJson.mapTo(UpdateToService.class);
+                System.out.println("TEST: La update to service = " + updateArrived.getLicense());
+                context.assertTrue(updateArrived.getPosition().equals(position)
+                        && updateArrived.getStatus().equals(state)
+                        && updateArrived.getLicense().equals(license)
+                        && updateArrived.getFailureMessage().equals(FAILURE_MESSAGE));
+            });
+            consumer.exceptionHandler(event -> {
+                context.fail(event.getCause());
+            });
+        });
+        vehicleVerticle.sendUpdate();
+        vertx.setTimer(5000, onTime -> {
+            System.out.println("TEST: ASYNC COMPLETE");
+            async.complete();});
+        async.awaitSuccess();
+    }
+
+}
