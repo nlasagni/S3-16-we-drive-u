5adc18344dd813eb6871592f624f315bbcee9be9
Michele Donati <michele.donati5@studio.unibo.it>
2017-08-11 17:16:58 +0200

    New Position class shared

--- shared/src/main/java/com/wedriveu/shared/utils/Position.java
+++ shared/src/main/java/com/wedriveu/shared/utils/Position.java
@@ -1,10 +1,10 @@
-package com.wedriveu.services.shared.utilities;
+package com.wedriveu.shared.utils;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * @author Marco Baldassarri
- * @author Michele
+ * @author Michele Donati
  * @since  17/07/2017
  */
 
@@ -44,4 +44,43 @@ public class Position {
                 + Math.pow(this.longitude - position.getLongitude(), 2));
     }
 
+    public static double getDistanceInKm(Position from, Position to) {
+        double earthRadius = 6372.795477598;
+        return earthRadius * Math.acos(Math.sin(from.getLatitude()) * Math.sin(to.getLatitude())
+                + Math.cos(from.getLatitude()) * Math.cos(to.getLatitude())
+                * Math.cos(from.getLatitude() - to.getLongitude()));
+    }
+
+    public double getDistanceInKm(Position to) {
+        double earthRadius = 6372.795477598;
+        return earthRadius * Math.acos(Math.sin(this.latitude) * Math.sin(to.getLatitude())
+                + Math.cos(this.latitude) * Math.cos(to.getLatitude())
+                * Math.cos(this.longitude - to.getLongitude()));
+    }
+
+    public static boolean isInRange(Position userPosition, Position vehiclePosition) {
+        return userPosition.getEuclideanDistance(vehiclePosition) < 20;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Position position = (Position) o;
+
+        if (Double.compare(position.latitude, latitude) != 0) return false;
+        return Double.compare(position.longitude, longitude) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(latitude);
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(longitude);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
 }
