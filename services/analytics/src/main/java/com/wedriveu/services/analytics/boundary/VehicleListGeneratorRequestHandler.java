e934d92b37196804cc6ea0a3f090b9c1037a1f7b
Stefano Bernagozzi <stefano.bernagozzi@studio.unibo.it>
2017-08-12 09:28:02 +0200

    added some changes

---
+++ services/analytics/src/main/java/com/wedriveu/services/analytics/boundary/VehicleListGeneratorRequestHandler.java
@@ -0,0 +1,46 @@
+package com.wedriveu.services.analytics.boundary;
+
+
+import com.wedriveu.services.shared.rabbitmq.VerticleConsumer;
+import com.wedriveu.services.shared.utilities.Log;
+import io.vertx.core.Future;
+import io.vertx.core.eventbus.Message;
+
+import static com.wedriveu.services.shared.utilities.Constants.*;
+
+/**
+ * @author Stefano Bernagozzi
+ */
+public class VehicleListGeneratorRequestHandler extends VerticleConsumer{
+
+    public VehicleListGeneratorRequestHandler() {
+        super("VehicleListGeneratorRequestHandler");
+    }
+
+    @Override
+    public void start(Future futureRequest) throws Exception {
+        super.start();
+        Future<Void> futureConsumer = Future.future();
+        futureConsumer.setHandler(v->{
+            if (v.succeeded()) {
+                Log.log("future in VehicleListGeneratorRequestHandler completed");
+                futureRequest.complete();
+            } else {
+                Log.error("future VehicleListGeneratorRequestHandler fail", v.cause().getLocalizedMessage(), v.cause());
+                futureRequest.fail(v.cause());
+            }
+        });
+        startConsumerWithFuture(ANALYTICS_SERVICE_EXCHANGE, ROUTING_KEY_VEHICLE_REQUEST_ALL, EVENT_BUS_AVAILABLE_ADDRESS, futureConsumer);
+
+    }
+
+    @Override
+    public void registerConsumer(String eventBus) {
+        Log.log("started vertx eventbus consumer in VehicleListGeneratorRequestHandler, attending start to receive");
+        vertx.eventBus().consumer(eventBus, this::sendToResponseHandler);
+    }
+
+    private void sendToResponseHandler(Message message) {
+        vertx.eventBus().send("mandaVeicoli", "manda");
+    }
+}
---
+++ services/analytics/src/main/java/com/wedriveu/services/analytics/boundary/VehicleListGeneratorResponseHandler.java
@@ -0,0 +1,56 @@
+package com.wedriveu.services.analytics.boundary;
+
+import com.wedriveu.services.shared.entity.Vehicle;
+import com.wedriveu.services.shared.rabbitmq.VerticlePublisher;
+import com.wedriveu.services.shared.utilities.Log;
+import com.wedriveu.services.shared.utilities.Position;
+import com.wedriveu.services.shared.vertx.VertxJsonMapper;
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.json.JsonObject;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import static com.wedriveu.services.shared.utilities.Constants.*;
+
+/**
+ * @author Stefano Bernagozzi
+ */
+public class VehicleListGeneratorResponseHandler extends VerticlePublisher{
+    @Override
+    public void start() throws Exception {
+        startConsumer();
+        Log.log("future VehicleListGeneratorResponseHandler complete");
+    }
+
+    private void startConsumer() {
+        Log.log("started vertx eventbus consumer in VehicleListGeneratorResponseHandler, attending start to receive");
+        vertx.eventBus().consumer("mandaVeicoli", this::sendVehicleListToAnalyticsService);
+    }
+
+
+    private void sendVehicleListToAnalyticsService(Message message) {
+        /*ArrayList<Vehicle> vehicleList = new ArrayList<>();
+        vehicleList.add(new Vehicle("MACCHINA1",
+                    "broken",
+                    new Position(10.2, 13.2),
+                    new Date(2017, 11, 30, 12, 37, 43)));
+                new Vehicle("MACCHINA2",
+                    "available",
+                    new Position(11.2, 14.2),
+                    new Date(2017, 10, 28, 11, 43, 12)),
+                new Vehicle("MACCHINA3",
+                    "busy",
+                    new Position(15.2, 13.2),
+                    new Date(2017, 9, 26, 10, 56, 46)),
+                new Vehicle("MACCHINA4",
+                    "recharging",
+                    new Position(13.2, 16.2),
+                    new Date(2017, 8, 24, 9, 37, 22)));*/
+        JsonObject vehicleListJson = VertxJsonMapper.mapInBodyFrom(new VehicleListObject());
+        System.out.println("VehicleListRequestVerticle " + vehicleListJson.toString());
+        publish(ANALYTICS_SERVICE_EXCHANGE,ROUTING_KEY_VEHICLE_RESPONSE_ALL,vehicleListJson);
+        Log.log("sent request for all vehicles to vehicle service in VehicleListGeneratorResponseHandler");
+    }
+
+}
