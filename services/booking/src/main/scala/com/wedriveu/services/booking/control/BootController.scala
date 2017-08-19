6964ed74925018bfa4cab1c1a2aed103c180be27
Nicola Lasagni <nicola.lasagni@gmail.com>
2017-08-19 10:43:47 +0200

    Added boot classes in order to boot all the components needed by the service

---
+++ services/booking/src/main/scala/com/wedriveu/services/booking/control/BootController.scala
@@ -0,0 +1,30 @@
+package com.wedriveu.services.booking.control
+
+import com.wedriveu.services.booking.util.BootManager
+import io.vertx.lang.scala.ScalaVerticle
+
+import scala.concurrent.Future
+
+/**
+  * @author Nicola Lasagni on 18/08/2017.
+  */
+object BootController {
+
+  /**
+    * The [[ScalaVerticle]] name that executes the boot operations
+    */
+  val Verticle: String = s"scala:${classOf[BootControllerImpl].getName}"
+
+  private class BootControllerImpl extends ScalaVerticle with BootManager {
+
+    override def startFuture(): Future[_] = {
+      boot()
+    }
+
+    override def boot(): Future[_] = {
+      vertx.deployVerticleFuture(BookingControllerVerticle.Verticle)
+    }
+
+  }
+
+}
