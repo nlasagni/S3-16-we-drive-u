fa0ef259045bcd51f6f4a9725b83407beaf579cf
Stefano Bernagozzi <stefano.bernagozzi@studio.unibo.it>
2017-08-20 09:27:24 +0200

    corrected vehicle service

--- services/booking/src/test/scala/com/wedriveu/services/booking/app/BookingServiceTestSimulation.scala
+++ services/booking/src/test/scala/com/wedriveu/services/booking/app/BookingServiceTestSimulation.scala
@@ -24,7 +24,7 @@ object BookingServiceTestSimulation {
     client.exchangeDeclareFuture(
       Shared.RabbitMQ.Exchanges.VEHICLE,
       Shared.RabbitMQ.Exchanges.Type.DIRECT,
-      durable = false,
+      durable = true,
       autoDelete = false
     ).flatMap(_ => {
       client.queueDeclareFuture(
